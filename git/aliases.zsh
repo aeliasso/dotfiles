9ad1587e0ef451c12e4bf6f66225982d6814ead6
Anton Eliasson <devel@antoneliasson.se>
2014-02-16 22:32:00 +0100

    Avbryt gpa vid den första remote som push misslyckas till

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,7 @@
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gpa='for x in $(git remote); do git push "$x" master; done'
+alias gpa='for x in $(git remote); do git push "$x" master || break; done'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
