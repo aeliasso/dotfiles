0cc34d3020211c1d18fb8f3fd0da1d028a86fd95
Anton Eliasson <devel@antoneliasson.se>
2014-11-13 15:07:17 +0100

    Fix for virtualenvwrapper in Ubuntu

--- python/python.zsh
+++ python/python.zsh
@@ -4,6 +4,8 @@ export PROJECT_HOME=$HOME/devel
 
 if [ -f /usr/bin/virtualenvwrapper.sh ]; then
     source /usr/bin/virtualenvwrapper.sh
+elif [ -f /usr/share/virtualenvwrapper/virtualenvwrapper.sh ]; then
+     source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 fi
 
 # låter pip cacha sina nerladdade paket (internetåtkomst krävs dock fortfarande)
