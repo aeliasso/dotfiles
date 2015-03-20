6cb6fc100f854a1c5dec40d474552c90fe0a8dda
Anton Eliasson <devel@antoneliasson.se>
2015-03-20 11:04:07 +0100

    Sensible local npm configuration.
    Ref: https://stackoverflow.com/questions/10081293/install-npm-into-home-directory-with-distribution-nodejs-package-ubuntu/13021677#13021677

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,7 +6,17 @@ fi
 # less själv och git (log) läser denna miljövariabel
 export LESS='--ignore-case --long-prompt --RAW-CONTROL-CHARS'
 
-export PATH="$HOME/.local/bin:$HOME/.rvm/bin:$PATH"
+# NPM packages in homedir
+export NPM_PACKAGES="$HOME/.npm-packages"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH  # delete if you already modified MANPATH elsewhere in your configuration
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
+# Tell Node about these packages
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+
+export PATH="$HOME/.local/bin:$HOME/.rvm/bin:$NPM_PACKAGES/bin:$PATH"
 
 # http://insanecoding.blogspot.se/2014/04/gcc-49-diagnostics.html
 export GCC_COLORS=1
