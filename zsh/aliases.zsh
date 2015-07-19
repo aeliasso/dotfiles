5b00e3e463e0aa6b385386939c21890ad099cee7
Anton Eliasson <devel@antoneliasson.se>
2015-07-19 18:21:43 +0200

    Deprecate screen in favour of tmux

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -33,10 +33,6 @@ function md () {
 # Detta behövs faktiskt:
 alias sudo='sudo '
 
-# Genväg till screen-sessioner
-alias programmera='screen -q -D -R programmera'
-alias administrera='screen -q -D -R administrera'
-
 # https://github.com/kyamagu/mexopencv/issues/74
 alias matlab='LD_PRELOAD=/usr/lib/libfreetype.so matlab'
 
