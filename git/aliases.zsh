3159a0b37e8dc0976eab661a5ab2aeb11b429624
Anton Eliasson <antone@axis.com>
2018-11-10 18:30:25 +0100

    git aliases: Remove unused aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -16,9 +16,4 @@ alias gpa='for x in $(git remote); do git push "$x" master || break; done'
 
 alias gd='git diff'
 alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
 alias gs='git status -sb' # let's not think too much about ghostscript (gs)
-alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
